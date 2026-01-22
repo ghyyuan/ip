9226fd67a521a852da957a85b4f707f8f286dd36
Gao Huiying <e1481825@u.nus.edu>
2026-01-22 14:03:06 +0800

    Memo.java: Add todo and list todo

--- src/main/java/Memo.java
+++ src/main/java/Memo.java
@@ -2,6 +2,8 @@ import java.util.*;
 
 public class Memo {
     public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+
         System.out.println("____________________________________________________________");
         System.out.println("Hello! I'm Memo\nWhat can I do for you?");
         System.out.println("____________________________________________________________");
@@ -10,10 +12,18 @@ public class Memo {
         String input = sc.nextLine();
         while (!input.equals("bye")) {
             System.out.println("____________________________________________________________");
-            System.out.println(input);
+            if (input.equals("list")) {
+                for (int i = 0; i < list.size(); i++) {
+                    System.out.println((i + 1) + ". " + list.get(i));
+                }
+            }else {
+                list.add(input);
+                System.out.println("added: " + input);
+            }
             System.out.println("____________________________________________________________");
             input = sc.nextLine();
         }
+        System.out.println("____________________________________________________________");
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println("____________________________________________________________");
     }
