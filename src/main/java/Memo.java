3dd50acce33c4d17e8a802a46aeebfc5101a6212
Gao Huiying <e1481825@u.nus.edu>
2026-01-22 13:37:38 +0800

    Memo.java: Echo user command

--- src/main/java/Memo.java
+++ src/main/java/Memo.java
@@ -1,8 +1,19 @@
+import java.util.*;
+
 public class Memo {
     public static void main(String[] args) {
         System.out.println("____________________________________________________________");
-        System.out.println("Hello! I'm Acaii\nWhat can I do for you?");
+        System.out.println("Hello! I'm Memo\nWhat can I do for you?");
         System.out.println("____________________________________________________________");
+
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        while (!input.equals("bye")) {
+            System.out.println("____________________________________________________________");
+            System.out.println(input);
+            System.out.println("____________________________________________________________");
+            input = sc.nextLine();
+        }
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println("____________________________________________________________");
     }
