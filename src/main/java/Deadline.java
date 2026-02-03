537ccca9a8678edb9cfd96e8f76dbc262670e784
Gao Huiying <e1481825@u.nus.edu>
2026-02-03 12:54:48 +0800

    Implement LocalDate in Deadline.java and Event.java

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,19 +1,29 @@
+import java.text.ParseException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Deadline extends Task {
 
-    protected String due;
+    protected LocalDate due;
 
-    public Deadline(String description, String due) {
+    public Deadline(String description, String due) throws MemoException {
         super(description);
-        this.due = due;
+        try {
+            this.due = LocalDate.parse(due);
+        } catch (Exception e) {
+            throw new MemoException("Please enter in yyyy-mm-dd format :O");
+        }
     }
 
     @Override
     public String toString() {
-        return String.format("[D]%s (by: %s)", super.toString(), due);
+        return String.format("[D]%s (by: %s)", super.toString(),
+                due.format(DateTimeFormatter.ofPattern("MMM d yyyy")));
     }
 
     @Override
     public String storeForm() {
-        return String.format("D | %s | %s", super.storeForm(), this.due);
+        return String.format("D | %s | %s", super.storeForm(),
+                this.due);
     }
 }
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,21 +1,31 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Event extends Task {
 
-    protected String from;
-    protected String to;
+    protected LocalDate from;
+    protected LocalDate to;
 
-    public Event(String description, String from, String to) {
+    public Event(String description, String from, String to) throws MemoException {
         super(description);
-        this.from = from;
-        this.to = to;
+
+        try {
+            this.from = LocalDate.parse(from);
+            this.to = LocalDate.parse(to);
+        } catch (Exception e) {
+            throw new MemoException("Please enter in yyyy-mm-dd format :O");
+        }
     }
 
     @Override
     public String toString() {
-        return String.format("[E]%s (from: %s to: %s)", super.toString(), from, to);
+        return String.format("[E]%s (from: %s to: %s)", super.toString(),
+                from.format(DateTimeFormatter.ofPattern("MMM d yyyy")), to.format(DateTimeFormatter.ofPattern("MMM d yyyy")));
     }
 
     @Override
     public String storeForm() {
-        return String.format("E | %s | %s | %s", super.storeForm(), this.from, this.to);
+        return String.format("E | %s | %s | %s", super.storeForm(),
+                this.from, this.to);
     }
 }
